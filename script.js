2b2c1133446652c872a15d737d2f975b3bc4b35c
dhandekorriyadh <abuhussain.dhan.dekor@gmail.com>
2025-03-12 03:11:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+    function showSection(section) {
+            // إخفاء كل الأقسام
+            var sections = document.querySelectorAll('.content');
+            sections.forEach(function (section) {
+                section.style.display = 'none';
+            });
 
+            // إظهار القسم المحدد
+            document.getElementById(section).style.display = 'block';
+        }
+
+        // عند تحميل الصفحة، نظهر القسم الرئيسي
+        window.onload = function () {
+            showSection('home');
+        }
